5aa7df772f359b46a3c66dd27b4a6cca00c2b065
mozhenhau <493842062@qq.com>
2015-05-15 15:45:26 +0800

    剩下D3View

---
+++ D3View/D3TextFiled.swift
@@ -0,0 +1,42 @@
+//
+//  D3TextFiled.swift
+//  D3View
+//
+//  Created by mozhenhau on 15/5/15.
+//  Copyright (c) 2015年 mozhenhau. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class D3TextField:UITextField,UITextFieldDelegate{
+    @IBInspectable var litmit: Int = 30  //为0则不限制
+    @IBInspectable var isGrayBorder: Bool = false {
+        didSet {
+            if isGrayBorder{
+                self.borderStyle = UITextBorderStyle.None
+                self.initStyle(ViewStyle.Border)
+                self.layer.borderColor = UIColor(red: 50/255 , green: 50/255, blue: 50/255, alpha: 0.8).CGColor
+            }
+        }
+    }
+    
+    required public init(coder aDecoder: NSCoder) {
+        super.init(coder:aDecoder)
+        delegate = self
+        if let style:ViewStyle = ViewStyle(rawValue: self.tag){
+            initStyle(style)
+        }
+    }
+    
+    func setLitmitCount(litmit:Int){
+        self.litmit = litmit
+    }
+    
+    public func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool{
+        if litmit != 0 && range.location > litmit{
+            return false
+        }
+        return true
+    }
+}
